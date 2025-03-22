cbd2fd2b8eeda16a371debd22129cc38660b3bce
Misha Voloshchuk <voloshchuk123@gmail.com>
2025-03-22 03:46:08 +0200

    Change links

--- projects/js/slides.js
+++ projects/js/slides.js
@@ -2,47 +2,47 @@ const slides = [
 	{ 
 		name: "Orbit Simulator", 
 		color: "#000000",
-		link: "Orbit-Simulator/",
+		link: "./Orbit-Simulator",
         image: 'img/orbitSimulator.jpg',
 	}, { 
 		name: "Buildspace", 
 		color: "#4A4CA3",
-		link: "Buildspace/" ,
+		link: "./Buildspace",
         image: 'img/buildSpace.jpg',
 	}, { 
 		name: "Deals Fest", 
 		color: "#2661A2",
-		link: "#",
+		link: "/#",
         image: 'img/dealsFest.jpg',
 	}, { 
 		name: "Golden", 
 		color: "#F5CA46",
-		link: "#",
+		link: "/#",
         image: 'img/golden.jpg',
 	}, { 
 		name: "Sushi Rolls", 
 		color: "#B844A9",
-		link: "#",
+		link: "/#",
         image: 'img/sushiRolls.jpg',
 	}, { 
 		name: "Portfolio", 
 		color: "#66DDAA",
-		link: "#",
+		link: "/#",
         image: 'img/portfolio.png',
 	}, { 
 		name: "Money Counter", 
 		color: "#181818",
-		link: "moneycounter/" ,
+		link: "./moneycounter",
         image: 'img/moneycounter.png',
 	}, { 
 		name: "Reaction Time Game", 
 		color: "#87F05D",
-		link: "Reaction-Time-Game/", 
+		link: "./Reaction-Time-Game",
         image: 'img/reaction-time-game.png',
 	}, { 
 		name: "Theremin", 
 		color: "#222222",
-		link: "Theremin/",
+		link: "./Theremin",
         image: 'img/Theremin.png',
 	}
 ];
